8f80a20ad80f1564415ecbc517745dedc34ac381
Alex Patino <patino@gmail.com>
2018-01-12 15:02:38 -0500

    Updated the scripts

--- fetch_cc_data.py
+++ fetch_cc_data.py
@@ -1,16 +1,28 @@
+import time
 import urllib
 import json
 import sys
+from optparse import OptionParser
 
-startVal = 1450936800
+usage = "usage: %prog [options] arg1 arg2"
+parser = OptionParser(usage=usage)
+parser.add_option("-s", "--startval", default=1450936800, dest="startVal",
+                  help="Unix Starting Timestamp")
+parser.add_option("-c", "--cointype", default = "BTC", dest="coinType",
+                  help="Cointypes: BTC, ETH, LTC, XRP")
+#https://www.cryptocompare.com/api/data/coinlist/ for more CTs
+(options, args) = parser.parse_args()
 
-for a in range (10):
-    url = "https://min-api.cryptocompare.com/data/histohour?fsym=BTC&tsym=USD&limit=2000&aggregate=1&e=CCCAGG&extraParams=apnyc&toTs=" + str(startVal)
+currentUnixTS = time.time()
+
+while options.startVal < currentUnixTS:
+    url = "https://min-api.cryptocompare.com/data/histohour?fsym=" + options.coinType + "&tsym=USD&limit=2000&aggregate=1&e=CCCAGG&extraParams=apnyc&toTs=" + str(options.startVal)
+    
     response = urllib.urlopen(url)
     data = json.loads(response.read())
 
-    fn = "data/cc_btc_hourly_" + str(a) + ".json"
+    fn = "data/cc_" + options.coinType + "_hourly_" + str(options.startVal) + ".json"
     with open(fn, 'w') as outfile:
         json.dump(data, outfile)
 
-    startVal += 7196400
+    options.startVal += 7196400
--- json_to_csv.py
+++ json_to_csv.py
@@ -1,5 +1,8 @@
+import time
+import datetime
 import json
 import csv
+import sys
 
 fileInput = "data\input.json"
 fileOutput = "data\output.csv"
@@ -12,9 +15,11 @@ f = open(fileInput)
 data = json.load(f)
 f.close()
 
-f = csv.writer(open(fileOutput))
+f = csv.writer(open(fileOutput, "wb+"))
 
 for x in data['Data']:
+    tv = x.get('time')
+    print ( datetime.datetime.fromtimestamp( tv ).strftime('%Y-%m-%d %H:%M:%S'))
     f.writerow ( x.values() )
 
     
